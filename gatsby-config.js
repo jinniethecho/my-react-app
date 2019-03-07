9cd411f16dae5ddff39632d09c0496aa0dcbf29d
Jinnie Cho <jinniethecho@gmail.com>
2019-03-07 21:48:17 +0900

    projects

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Yejin Cho',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hi there! I'm Yejin. And I'm..</h1>
+    <p>interaction designer, prototyper, and so on. </p>
+    <Link to="/page-2/">Go </Link>
   </div>
 )
 
